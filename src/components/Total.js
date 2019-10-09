0f676d5ae5078aeb537e45198db95dac12131462
ofega <fegaeze@gmail.com>
2019-10-09 20:30:27 +0100

    cleanup of console logs

--- src/components/Total.js
+++ src/components/Total.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 export const Total = ({ state }) => {
-  console.log(state);
-  console.log(state.car.price, state.additionalPrice)
   return (
     <div className="content">
       <h4>Total Amount: ${state.car.price + state.additionalPrice}</h4>
